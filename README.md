c5e0c8f02051e0408c1587fb93bde9a578f098aa
Josh Frank <eurosewusa@gmail.com>
2021-07-24 21:47:22 -0400

    adding CLI and updating readme

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # path-normalizer
 
-A tool to parse SVG paths and normalize them. Accompanies my blog article, part of a series, on SVG paths.
+A tool to parse absolute SVG paths and normalize them. Accompanies my blog article, part of a series, on SVG paths. **Right now this code works only with absolute paths.**
+
+## Using this code
+
+First, `require()` `PathParser` and `NormalizedPath` at the top of your code. Then construct a new `NormalizedPath` object with a descriptor as a string like so:
+
+```
+const testNormalizedPath = new NormalizedPath( "M 300 300 A 50 50 90 1 1 400 300 A 50 50 90 1 1 300 300 z" )
+console.log( testNormalizedPath.toString() )
+--> M 300 300 C 300,272.4042487753,322.4042487753,250,350,250 377.5957512247,250,400,272.4042487753,400,300 C 400,327.5957512247,377.5957512247,350,350,350 322.4042487753,350,300,327.5957512247,300,300 Z
+```
+
+Use the included cli with `yarn test "<path descriptor>"` or `npm test "<path descriptor>"`.
--- cli.js
+++ cli.js
@@ -7,10 +7,27 @@
 //     "m 628 356 l 192 -224 l 96 -32 l -32 96 l -224 192 c 32 32 32 64 64 30 c 0 32 32 64 0 64 a 45.44 45.44 90 0 1 -32 32 a 160 160 90 0 0 -64 -96 q -16 -3.2 -16 16 t -48 41.6 t -25.6 -25.6 t 41.6 -48 t 16 -16 a 160 160 90 0 0 -96 -64 a 45.44 45.44 90 0 1 32 -32 c 0 -32 32 0 64 0 c -32 32 0 32 32 66 l 192 -224 l 0 64 l 64 0 l -57.6 -6.4 l -6.4 -57.6 z"
 // ];
 
+
+"use strict";
+
 const { PathParser, NormalizedPath } = require(".");
 
-// testShapes.forEach( shape => console.log( PathParser.parseRaw( shape ) ) );
-// console.log( new NormalizedPath( "m 50,50 l 100,0 l 0,100 l -100,0 z" ).toString() );
-// console.log( new NormalizedPath( "M 80 508 S 150 454 140 370 S 206 272 290 320 z" ).toString() );
-console.log( new NormalizedPath( "M 300 300 A 50 50 90 1 1 400 300 A 50 50 90 1 1 300 300 z" ).toString() );
-// console.log( new NormalizedPath( "m 80 508 s 70 -54 60 -138 s 66 -98 150 -50 z" ).toString() );
+/* eslint no-process-exit: 0 */
+/* eslint global-require: 0 */
+
+if ( require.main === module ) {
+  let sliceN = 1;
+  if ( process.argv.indexOf( module.filename ) > -1 || require( "path" ).basename( process.argv[ 1 ] ) === "path-parser" ) {
+    sliceN = 2;
+  }
+  const args = process.argv.slice( sliceN, process.argv.length );
+
+  if ( args.length === 0 ) {
+    console.error( "You must pass SVG paths enclosed between quotes as parameters." );
+    process.exit( 1 );
+  }
+
+  for ( let a = 0; a < args.length; a++ ) {
+    console.log( new NormalizedPath( args[ a ] ).toString() );
+  }
+}
